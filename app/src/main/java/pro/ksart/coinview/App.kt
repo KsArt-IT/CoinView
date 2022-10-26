6e217f6a82fd605e9d1c208af7e6763ab2f60616
KsArT-IT <ksart.it@gmail.com>
2022-10-26 20:06:11 +0300

    feat: add package structure

---
+++ app/src/main/java/pro/ksart/coinview/App.kt
@@ -0,0 +1,7 @@
+package pro.ksart.coinview
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/pro/ksart/coinview/presentation/MainActivity.kt
+++ app/src/main/java/pro/ksart/coinview/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package pro.ksart.coinview
+package pro.ksart.coinview.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pro.ksart.coinview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
